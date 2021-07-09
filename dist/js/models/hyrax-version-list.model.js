c44efb04c06fcc474fb16ebf7f8512e10e314b3c
porrello <alexander.porrello@kapsch.net>
2021-07-09 14:27:58 -0500

    Pushed new web data.

--- dist/js/models/hyrax-version-list.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HyraxVersionList = void 0;
-var HyraxVersionList = /** @class */ (function () {
-    function HyraxVersionList() {
-    }
-    return HyraxVersionList;
-}());
-exports.HyraxVersionList = HyraxVersionList;
